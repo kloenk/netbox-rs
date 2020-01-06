b53820acbedaabd81fc80c55dc48c5c2c58cb3bb
Finn Behrens <me@kloenk.de>
2020-01-06 21:17:14 +0100

    more on ipam_ip_addresses()

--- src/structs.rs
+++ src/structs.rs
@@ -59,9 +59,11 @@ impl<T> Response<T> {
 pub struct IPAddress {
     pub id: usize,
     pub address: std::net::IpAddr,
+    pub mask: u8,
     pub family: Choice,
     pub vrf: Option<Link>,
     pub tenant: Option<Link>,
+    pub status: Choice,
     // pub role:
     pub interface: Option<IPAddressInterface>,
     // pub nat_inside
@@ -75,8 +77,8 @@ pub struct IPAddress {
 }
 
 impl IPAddress {
-    pub fn parse(Value: &serde_json::Value) -> Result<IPAddress> {
-        let v: &serde_json::Value = Value;
+    pub fn parse(value: &serde_json::Value) -> Result<IPAddress> {
+        let v: &serde_json::Value = value;
         let id: usize = v
             .get("id")
             .map(|v| v.as_i64())
@@ -89,9 +91,102 @@ impl IPAddress {
                 .ok_or_else(|| Box::<dyn Error>::from("family missing".to_string()))?,
         )?;
 
-        println!("val: {:?}", family);
+        let (address, mask) = v
+            .get("address")
+            .map(|v| v.as_str())
+            .flatten()
+            .map(|v| v.split('/').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse address".to_string()))?;
+        let address: Result<std::net::IpAddr> = {
+            use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+            if address.contains(':') {
+                let address = address.replace("::", ":fill:");
+                let address = address.split(':').collect::<Vec<&str>>();
+                let mut buff = Vec::new();
+                let mut c = 9 - address.len();
 
-        unimplemented!()
+                for v in address {
+                    if v == "fill" {
+                        for _ in 0..c {
+                            buff.push(0);
+                        }
+                    } else {
+                        let v = i32::from_str_radix(v, 16)? as u16;
+                        buff.push(v)
+                    }
+                }
+                Ok(IpAddr::V6(Ipv6Addr::new(
+                    buff[0], buff[1], buff[2], buff[3], buff[4], buff[5], buff[6], buff[7],
+                )))
+            } else {
+                let v = address.split(".").collect::<Vec<&str>>();
+                if v.len() != 4 {
+                    return Err(Box::<dyn Error>::from(
+                        "could not parse address".to_string(),
+                    ));
+                }
+                Ok(IpAddr::V4(Ipv4Addr::new(
+                    v[0].parse()?,
+                    v[1].parse()?,
+                    v[2].parse()?,
+                    v[3].parse()?,
+                )))
+            }
+        };
+        let address = address?;
+        let mask: u8 = mask.parse()?;
+
+        let vrf: Option<Link> = v.get("vrf").map(|v| Link::parse(v).ok()).flatten();
+        let tenant: Option<Link> = v.get("tenant").map(|v| Link::parse(v).ok()).flatten();
+        let status: Choice = Choice::parse(
+            v.get("status")
+                .ok_or_else(|| Box::<dyn Error>::from("status missing".to_string()))?,
+        )?;
+
+        // FIMXE: role
+
+        let interface: Option<IPAddressInterface> = v
+            .get("interface")
+            .map(|v| IPAddressInterface::parse(v).ok())
+            .flatten();
+
+        // FIXME: nat
+        let dns_name: String = v
+            .get("dns_name")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .unwrap_or_else(|| String::new());
+
+        let description: String = v
+            .get("description")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .unwrap_or_else(|| String::new());
+
+        let tags: Vec<String> = Vec::new(); // TODO
+        let created: String = String::new(); // TODO
+        let updated: String = String::new(); // TODO
+
+        Ok(Self {
+            id,
+            family,
+            address,
+            mask,
+            vrf,
+            tenant,
+            status,
+            //role
+            interface,
+            //nat
+            //nat
+            dns_name,
+            description,
+            tags,
+            //custom
+            created,
+            updated,
+        })
     }
 }
 
@@ -104,10 +199,86 @@ pub struct IPAddressInterface {
     pub name: String,
 }
 
+impl IPAddressInterface {
+    pub fn parse(value: &serde_json::Value) -> Result<Self> {
+        let id: usize = value
+            .get("id")
+            .map(|v| v.as_i64().map(|v| v as usize))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse id".to_string()))?;
+        let url: String = value
+            .get("url")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse url".to_string()))?;
+        let name: String = value
+            .get("name")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse name".to_string()))?;
+        let device: Option<Link> = value.get("device").map(|v| Link::parse(v).ok()).flatten();
+        let virtual_machine: Option<Link> = value
+            .get("virtual_machine")
+            .map(|v| Link::parse(v).ok())
+            .flatten();
+        Ok(Self {
+            id,
+            url,
+            name,
+            device,
+            virtual_machine,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct Link {
     pub id: usize,
     pub url: String,
     pub name: String,
     pub extra: (String, Option<String>),
+}
+
+impl Link {
+    pub fn parse(value: &serde_json::Value) -> Result<Self> {
+        let id: usize = value
+            .get("id")
+            .map(|v| v.as_i64().map(|v| v as usize))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse id".to_string()))?;
+        let url: String = value
+            .get("url")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse url".to_string()))?;
+        let name: String = value
+            .get("name")
+            .map(|v| v.as_str().map(|v| v.to_string()))
+            .flatten()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse name".to_string()))?;
+        let value = value
+            .as_object()
+            .ok_or_else(|| Box::<dyn Error>::from("could not parse extra".to_string()))?;
+
+        let mut iter = value
+            .iter()
+            .filter(|x| x.0 != "id" && x.0 != "url" && x.0 != "name");
+
+        if let Some((k, v)) = iter.next() {
+            let v = v.as_str().map(|v| v.to_string());
+            return Ok(Self {
+                id,
+                url,
+                name,
+                extra: (k.to_string(), v),
+            });
+        }
+
+        return Ok(Self {
+            id,
+            url,
+            name,
+            extra: (String::new(), None),
+        });
+    }
 }
